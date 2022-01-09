bede574a9a09290de44b956287ef92e7510ce3d1
manodhiambo <manodhiambo@gmail.com>
2022-01-09 00:08:05 -0800

    fixing bugs

--- sort.h
+++ sort.h
@@ -1,5 +1,5 @@
-#ifndef SORT_H
-#define SORT_H
+#ifndef SORTING
+#define SORTING
 
 
 #include <stddef.h>
@@ -44,4 +44,4 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
-#endif /* SORT_H */
+#endif /* SORTING */
