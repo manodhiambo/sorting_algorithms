00e812bbe7598c1099332db5f3a8636381af4bdc
manodhiambo <manodhiambo@gmail.com>
2022-01-08 22:45:26 -0800

    More header files

--- sort.h
+++ sort.h
@@ -28,6 +28,15 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
+
 
 /* Custom functions */
 int part(int *array, int left, int right, size_t size);
